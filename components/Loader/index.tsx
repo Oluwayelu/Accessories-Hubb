46a63d2566712ac22852308b095a85a458ce544a
oluwayelu <oluwayeluifeoluwa@gmail.com>
2022-09-05 02:48:05 +0100

    feat: changed Loading text to a Spinner

--- components/Loader/index.tsx
+++ components/Loader/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Loader = () => {
   return (
     <div className="h-screen w-full flex items-center justify-center bg-gray-200">
-      <p>Loading...</p>
+      <div className="w-16 h-16 border-b-2 border-primary border-dotted rounded-full animate-spin" />
     </div>
   );
 };

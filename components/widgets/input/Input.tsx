74c565c8fbd037fa545fe2ff7b84983cc0e1d44e
oluwayelu <oluwayeluifeoluwa@gmail.com>
2023-03-17 21:30:42 +0100

    fix: Input field for password type

--- components/widgets/input/Input.tsx
+++ components/widgets/input/Input.tsx
@@ -15,35 +15,37 @@ type Props = {
 const Input = ({ label, formik, caption, ...props }: Props) => {
   const [password, setPassword] = useState<boolean>(true);
 
-  if (props.type === "password") {
-    return (
-      <div
-        className={`${props.className} w-full h-12 flex items-center justify-between px-3 border-2 bg-white border-gray-400 focus:border-primary rounded-xl disabled:bg-gray-200 space-x-3`}
-      >
-        <input
-          {...props}
-          type={password ? "password" : "text"}
-          className="w-full h-full bg-transparent"
-        />
-        <div className="cursor-pointer" onClick={() => setPassword(!password)}>
-          {password ? (
-            <FaEye className="text-gray-400" />
-          ) : (
-            <FaEyeSlash className="text-gray-400" />
-          )}
-        </div>
-      </div>
-    );
-  }
-
   return (
     <div className={`${props.className} w-full flex flex-col`}>
       {label && <label className="text-sm font-semibold">{label}</label>}
-      <input
-        {...props}
-        className="px-3 h-12 border-2 border-gray-400 focus:border-primary rounded-xl disabled:bg-gray-200"
-      />
-      {caption && <p className="text-xs text-left">{caption}</p>}
+      {props.type === "password" ? (
+        <div
+          className={`${props.className} w-full h-12 flex items-center justify-between px-3 border-2 bg-white border-gray-400 focus:border-primary rounded-xl disabled:bg-gray-200 space-x-3`}
+        >
+          <input
+            {...props}
+            type={password ? "password" : "text"}
+            className="w-full h-full bg-transparent"
+          />
+          <div
+            className="cursor-pointer"
+            onClick={() => setPassword(!password)}
+          >
+            {password ? (
+              <FaEye className="text-gray-400" />
+            ) : (
+              <FaEyeSlash className="text-gray-400" />
+            )}
+          </div>
+        </div>
+      ) : (
+        <input
+          {...props}
+          className="px-3 h-12 border-2 border-gray-400 focus:border-primary rounded-xl disabled:bg-gray-200"
+        />
+      )}
+
+      {caption && <p className="text-xs text-left text-gray-400">{caption}</p>}
       {formik && props.name && (
         <ErrorMessage
           name={props.name}
